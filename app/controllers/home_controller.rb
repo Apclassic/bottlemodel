312dab12106ed942b703c5cd2a5cd9bf647e9d30
apclassic <brodeybriggs@hotmail.com>
2023-02-20 23:48:06 -0600

    adding new view for id pages, and adding related routes and db

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,6 @@ class HomeController < ApplicationController
   def index
     @homes = Home.all
   end
+  def show 
+    @home = Home.find(parms[:id])
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "home#index"
   
   get "/", to: "home#index"
+  get "/:id", to: "home#show"
 end
